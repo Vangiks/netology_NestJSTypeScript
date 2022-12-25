7afd0c035ad7fc9c272c603b967b7d0dacbe6fb7
Vyduikin <i.vyduikin@mkskom.ru>
2022-12-25 14:16:55 +0300

    fix: Убрать try catch из модуля "Отель"

--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -8,76 +8,46 @@ import { ICreateHotel, ISearchHotelParams, IUpdateHotel } from './dto';
 
 @Injectable()
 export class HotelService {
-  constructor(
-    @InjectModel(Hotel.name) private HotelModel: Model<Hotel>,
-  ) {}
+  constructor(@InjectModel(Hotel.name) private HotelModel: Model<Hotel>) {}
 
-  async create(data: ICreateHotel): Promise<Hotel | null> {
-    try {
-      const hotel: IHotel = {
-        title: data.title,
-        description: data.description,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-      const newHotel: Hotel = new this.HotelModel(hotel);
-      return newHotel.save();
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
-    }
+  async create(data: ICreateHotel): Promise<Hotel> {
+    const hotel: IHotel = {
+      title: data.title,
+      description: data.description,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const newHotel: Hotel = new this.HotelModel(hotel);
+    return newHotel.save();
   }
 
-  async findById(id: TID): Promise<Hotel | null> {
-    try {
-      return this.HotelModel.findById(id).select('-__v');
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
-    }
+  async findById(id: TID): Promise<Hotel> {
+    return this.HotelModel.findById(id).select('-__v');
   }
 
   async search(
     params: ISearchHotelParams,
     select?: string,
-  ): Promise<Array<Hotel> | null> {
-    try {
-      const filter = {
-        title: { $regex: params.title },
-      };
-      return this.HotelModel.find(filter)
-        .select('-__v' + select ? ' ' + select : '')
-        .skip(params.offset)
-        .limit(params.limit);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
-    }
+  ): Promise<Array<Hotel>> {
+    const filter = {
+      title: { $regex: params.title },
+    };
+    return this.HotelModel.find(filter)
+      .select('-__v' + select ? ' ' + select : '')
+      .skip(params.offset)
+      .limit(params.limit);
   }
 
   async update(
     id: TID,
     data: IUpdateHotel,
     options: QueryOptions = { new: false },
-  ): Promise<Hotel | null> {
-    try {
-      const updateHotel = {
-        title: data.title,
-        description: data.description,
-        updatedAt: new Date(),
-      };
-      return this.HotelModel.findByIdAndUpdate(id, updateHotel, options);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
-    }
+  ): Promise<Hotel> {
+    const updateHotel = {
+      title: data.title,
+      description: data.description,
+      updatedAt: new Date(),
+    };
+    return this.HotelModel.findByIdAndUpdate(id, updateHotel, options);
   }
 }
