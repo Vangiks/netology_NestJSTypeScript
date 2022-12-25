01073bd9477bc0299fe5f904efb2352fb6ba86a1
Vyduikin <i.vyduikin@mkskom.ru>
2022-12-25 14:53:19 +0300

    build: Убрать тип, ошибка build

--- src/common/pipes/multer-file-base64.pipe.ts
+++ src/common/pipes/multer-file-base64.pipe.ts
@@ -2,7 +2,7 @@ import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 
 @Injectable()
 export class MulterFileBase64Pipe implements PipeTransform {
-  transform(files: Array<Express.Multer.File>, metadata: ArgumentMetadata) {
+  transform(files, metadata: ArgumentMetadata) {
     if (files.length) {
       return files.map((file) => file.buffer.toString('base64'));
     }
