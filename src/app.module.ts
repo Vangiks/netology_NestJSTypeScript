0a5dd7c422cdb7dd2a29c75f8d8ab018d3eb874e
Vyduikin <i.vyduikin@mkskom.ru>
2022-12-25 14:53:38 +0300

    build: Убрать тестирование, ошибка build

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import { HotelModule } from './hotel/hotel.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { SupportRequestModule } from './support-request/support-request.module';
@@ -23,18 +22,9 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
-        const nodeEnv = configService.get<string>('NODE_ENV');
-        if (nodeEnv === 'test') {
-          const mongod = await MongoMemoryServer.create();
-          const uri = mongod.getUri();
-          return {
-            uri: uri,
-          };
-        } else {
-          return {
-            uri: configService.get<string>('DATABASE_PATH'),
-          };
-        }
+        return {
+          uri: configService.get<string>('DATABASE_PATH'),
+        };
       },
       inject: [ConfigService],
     }),
