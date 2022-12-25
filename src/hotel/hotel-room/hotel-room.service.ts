66eb6a9187618f3d90f30f4260e05da886d212b5
Vyduikin <i.vyduikin@mkskom.ru>
2022-12-25 14:22:38 +0300

    fix: Убрать try catch из модуля "Отель"

--- src/hotel/hotel-room/hotel-room.service.ts
+++ src/hotel/hotel-room/hotel-room.service.ts
@@ -17,77 +17,47 @@ export class HotelRoomService {
     private HotelRoomModel: Model<HotelRoom>,
   ) {}
 
-  async create(data: ICreateHotelRoom): Promise<HotelRoom | null> {
-    try {
-      const hotelRoom: IHotelRoom = {
-        hotel: data.hotelId,
-        description: data.description,
-        images: data.images,
-        isEnabled: true,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-      const newHotelRoom: HotelRoom = new this.HotelRoomModel(
-        hotelRoom,
-      );
-      return newHotelRoom.save();
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
-    }
+  async create(data: ICreateHotelRoom): Promise<HotelRoom> {
+    const hotelRoom: IHotelRoom = {
+      hotel: data.hotelId,
+      description: data.description,
+      images: data.images,
+      isEnabled: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const newHotelRoom: HotelRoom = new this.HotelRoomModel(hotelRoom);
+    return newHotelRoom.save();
   }
 
-  async findById(id: TID): Promise<HotelRoom | null> {
-    try {
-      return this.HotelRoomModel.findById(id).select('-__v');
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
-    }
+  async findById(id: TID): Promise<HotelRoom> {
+    return this.HotelRoomModel.findById(id).select('-__v');
   }
 
   async search(
     params: ISearchHotelRoomsParams,
     select?: string,
-  ): Promise<Array<HotelRoom> | null> {
-    try {
-      if (params.isEnabled) {
-        params.isEnabled = params.isEnabled;
-      }
-      const selectedHotelRoom = this.HotelRoomModel.find(params).select('-__v');
-
-      return selectedHotelRoom
-        .select('-__v' + select ? ' ' + select : '')
-        .skip(params.offset)
-        .limit(params.limit);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
+  ): Promise<Array<HotelRoom>> {
+    if (params.isEnabled) {
+      params.isEnabled = params.isEnabled;
     }
+    const selectedHotelRoom = this.HotelRoomModel.find(params).select('-__v');
+
+    return selectedHotelRoom
+      .select('-__v' + select ? ' ' + select : '')
+      .skip(params.offset)
+      .limit(params.limit);
   }
 
   async update(
     id: TID,
     data: IUpdateHotelRoom,
     options: QueryOptions = { new: false },
-  ): Promise<HotelRoom | null> {
-    try {
-      return this.HotelRoomModel.findByIdAndUpdate(
-        id,
-        { ...data, updatedAt: new Date() },
-        options,
-      );
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.log(error.message);
-      }
-      return null;
-    }
+  ): Promise<HotelRoom> {
+    return this.HotelRoomModel.findByIdAndUpdate(
+      id,
+      { ...data, updatedAt: new Date() },
+      options,
+    );
   }
 }
